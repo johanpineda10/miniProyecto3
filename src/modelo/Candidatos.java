6c7120f2680bdc976bbab9bde748966416b5d28b
JAlexis1 <144349234+JAlexis1@users.noreply.github.com>
2023-11-30 17:33:29 -0500

    Add files via upload

--- src/modelo/Candidatos.java
+++ src/modelo/Candidatos.java
@@ -1,40 +1,75 @@
 package modelo;
 
+// Candidato.java
+import java.util.Objects;
+
 public class Candidatos {
-    private String nombre, cedula;
-    private int ideologia;
-    private Ciudades ciudades;
-    private Partidos partido;
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
+    private String nombre;
+    private int cedula;
+    private String ciudad;
+    private String partido;
+    public int votos;
+
+    public Candidatos(String nombre, int cedula, String ciudad, String partido) {
         this.nombre = nombre;
-    }
-    public String getCedula() {
-        return cedula;
-    }
-    public void setCedula(String cedula) {
         this.cedula = cedula;
-    }
-    public int getIdeologia() {
-        return ideologia;
-    }
-    public void setIdeologia(int ideologia) {
-        this.ideologia = ideologia;
-    }
-    public Ciudades getCiudades() {
-        return ciudades;
-    }
-    public void setCiudades(Ciudades ciudades) {
-        this.ciudades = ciudades;
-    }
-    public Partidos getPartido() {
-        return partido;
-    }
-    public void setPartido(Partidos partido) {
+        this.ciudad = ciudad;
         this.partido = partido;
     }
 
-    
+    public Candidatos() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Candidatos candidato = (Candidatos) o;
+        return cedula == candidato.cedula;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cedula);
+    }
 }
--- src/vistas/vistaCandidatos.java
+++ src/vistas/vistaCandidatos.java
@@ -1,11 +1,20 @@
 package vistas;
 
+import controlador.ControladorCandidatos;
+import modelo.Ciudades;
+import modelo.Partidos;
+
 public interface vistaCandidatos {
-    public void iniciar();
+    public void iniciar(ControladorCandidatos candidato);
     public String getNombre();
     public String getCedula();
-    public String getPartido();
-    public String getCiudad();
-    public String getIdeologia();
+    public Partidos getPartido();
+    public Ciudades getCiudad();
+    public int getIdeologia();
+    public void setNombre();
+    public void setCedula();
+    public void setIdeologia();
+    public void setPartido();
+    public void setCiudades();
     
 }
