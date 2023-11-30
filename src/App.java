86dc606390c8e5a1fc7341cbcb889d7ef40a7f6a
johanpineda10 <johan.laverde@correounivalle.edu.co>
2023-11-30 12:52:17 -0500

    organizacion vista GUI

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 import controlador.ControladorCandidatos;
 import modelo.Candidatos;
 import vistas.vistaCandidatoConsola;
@@ -6,9 +8,28 @@ import vistas.vistaCandidatoGUI;
 public class App {
     public static void main(String[] args) throws Exception {
         Candidatos candi = new Candidatos();
-        //vistaCandidatoConsola vistaConsola = new vistaCandidatoConsola();
+        vistaCandidatoConsola vistaConsola = new vistaCandidatoConsola();
         vistaCandidatoGUI vistaGUI = new vistaCandidatoGUI();
-        ControladorCandidatos control = new ControladorCandidatos(candi, vistaGUI);
-        control.iniciarVista();
+        
+        
+        String[] opciones = {"Interfaz Gráfica (GUI)", "Consola"};
+        int seleccion = JOptionPane.showOptionDialog(
+                null,
+                "Selecciona la forma de ejecución:",
+                "Ejecutar en",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                opciones,
+                opciones[0]
+        );
+
+        if (seleccion == 0) {
+            ControladorCandidatos control = new ControladorCandidatos(candi, vistaGUI);
+            control.iniciarVista();
+        } else {
+            ControladorCandidatos control = new ControladorCandidatos(candi, vistaConsola);
+            control.iniciarVista();
+        }
     }
 }
